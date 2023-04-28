5b83a0954191f645f1127f044dbd28060cdcbcab
ManuelF <manuel.floriani1999@gmail.com>
2023-04-28 15:35:48 +0200

    function calcola()

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ const euroPerChilometro = 0.21;
 
 // immettere i chilometri da percorrere
 function prendiChilometri(){
-    userChilometri = document.getElementById("chilometri-utente").value = "";
+    userChilometri = document.getElementById("chilometri-utente").value;
 }
     console.log("Chilometri inseriti dall'utente: " + prendiChilometri());
 
@@ -18,7 +18,7 @@ function prendiChilometri(){
 
 // immettere l'età del passeggero
 function prendiEta(){
-    userEta = document.getElementById("eta-utente").value = "";
+    userEta = document.getElementById("eta-utente").value;
 }
 
 console.log("Età inserita dall'utente: " + prendiEta());
@@ -28,24 +28,25 @@ userTotaleParziale = userChilometri * euroPerChilometro;
 
 // calcolare lo sconto
 // let arrotondamento = userTotale.toFixed(2);
+function calcola(){
+    
+    // se età < 18 anni sconto 20%
+    if(userEta < 18){
+        userTotale = userTotaleParziale - (userTotaleParziale * 20 / 100);
+        messaggio = `Giovanotto il prezzo del tuo biglietto è ${userTotale.toFixed(2)}€`;
+    }
 
-// se età < 18 anni sconto 20%
-if(userEta < 18){
-    userTotale = userTotaleParziale - (userTotaleParziale * 20 / 100);
-    messaggio = `Giovanotto il prezzo del tuo biglietto è ${userTotale.toFixed(2)}€`;
+    // se età > 65 anni sconto 40%
+    else if(userEta >= 65){
+        userTotale = userTotaleParziale - (userTotaleParziale * 40 / 100);
+        messaggio = `Nonno il prezzo del tuo biglietto è ${userTotale.toFixed(2)}€`;
+    }
+
+    // età compresa tra 18 e 64 senza sconto
+    else{
+        userTotale = userTotaleParziale;
+        messaggio = `Il prezzo del tuo biglietto è ${userTotale.toFixed(2)}€`;
+    }
+
+    document.getElementById("mio_id").innerHTML = messaggio;
 }
-
-// se età > 65 anni sconto 40%
-else if(userEta >= 65){
-    userTotale = userTotaleParziale - (userTotaleParziale * 40 / 100);
-    messaggio = `Nonno il prezzo del tuo biglietto è ${userTotale.toFixed(2)}€`;
-}
-
-// età compresa tra 18 e 64 senza sconto
-else{
-    userTotale = userTotaleParziale;
-    messaggio = `Il prezzo del tuo biglietto è ${userTotale.toFixed(2)}€`;
-}
-
-
-document.getElementById("mio_id").innerHTML = messaggio;
